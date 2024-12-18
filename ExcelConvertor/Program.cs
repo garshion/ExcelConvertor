5f446fad12cbb77e50eaf49f4a42209a98bb005b
BassGoon <garshion@gmail.com>
2024-12-18 12:24:45 +0900

    기본 프로젝트 뼈대 추가

---
+++ ExcelConvertor/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Windows.Forms;
+
+namespace Bass.Tools
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+
+#if NETFRAMEWORK
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+#else
+            ApplicationConfiguration.Initialize();
+#endif
+            Application.Run(new frmMain());
+        }
+    }
+}
---
+++ ExcelConvertor/frmMain.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Bass.Tools
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
